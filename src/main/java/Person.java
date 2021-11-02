896cd0efc806a32d9256fe0000c49746f81bc39f
igoreta <ivoronline@gmail.com>
2021-11-02 14:57:15 +0100

    Initial commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,11 @@
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Person {
+
+  //PROPERTIES
+  public Integer id;
+  public String  name;
+  public Integer age;
+
+}
